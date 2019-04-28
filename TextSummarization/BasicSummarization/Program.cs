5f089c2df5be80decaf155bd2b17772d5e21c620
Eduardo Larios <eduardo.larios.fdz@outlook.com>
2019-04-28 15:58:58 -0500

    Added base solution and projects

---
+++ TextSummarization/BasicSummarization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicSummarization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
