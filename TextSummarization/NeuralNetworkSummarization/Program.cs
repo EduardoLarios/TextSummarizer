fad001d7dbcc30c0f3d3f7cba8684258bc4371de
Eduardo Larios <eduardo.larios.fdz@outlook.com>
2019-05-02 21:26:55 -0500

    testing writing to a directory

--- TextSummarization/NeuralNetworkSummarization/Program.cs
+++ TextSummarization/NeuralNetworkSummarization/Program.cs
@@ -14,14 +14,14 @@ namespace NeuralNetworkSummarization
         {
             string filePath = "Assets/stopwords-english.txt";
             var text = File.ReadAllLines(filePath);
-            var F1 = new List<double>( );
+            var F1 = new List<double>();
             var trailingCharacters = new char[] { ',', '"', '\'' };
 
             foreach (var item in noWhiteSpace)
             {
                 double f1Value = 0;
                 var words = item.Split(' ');
-                var clean = new List<string>( );
+                var clean = new List<string>();
 
                 foreach (var word in words)
                 {
@@ -55,8 +55,8 @@ namespace NeuralNetworkSummarization
         static List<int> ExtractFeature2(List<string> cleanedUpSentences)
         {
             int count = 0;
-            var F2 = new List<int>( );
-            foreach (var word in cleanedUpSentences.First( ).Split(' '))
+            var F2 = new List<int>();
+            foreach (var word in cleanedUpSentences.First().Split(' '))
             {
                 foreach (var word2 in cleanedUpSentences[1].Split(' '))
                 {
@@ -85,7 +85,7 @@ namespace NeuralNetworkSummarization
                 F2.Add(contar);
             }
 
-            foreach (var lastWord in cleanedUpSentences.Last( ).Split(' '))
+            foreach (var lastWord in cleanedUpSentences.Last().Split(' '))
             {
                 foreach (var word2 in cleanedUpSentences[cleanedUpSentences.Count - 2].Split(' '))
                 {
@@ -100,7 +100,7 @@ namespace NeuralNetworkSummarization
         static int ExtractFeature3(IEnumerable<string> wordsInText, string sentence)
         {
             var counter = 0;
-            var uniqueWords = new HashSet<string>( );
+            var uniqueWords = new HashSet<string>();
             foreach (var word in sentence.Split(' '))
             {
                 if (!uniqueWords.Contains(word))
@@ -159,7 +159,7 @@ namespace NeuralNetworkSummarization
 
         static double ExtractFeature7(IEnumerable<string> text, string sentence)
         {
-            var numSentences = text.Count( );
+            var numSentences = text.Count();
             var average = text.Sum(s => s.Length) / numSentences;
 
             return (double) sentence.Length / average;
@@ -167,7 +167,7 @@ namespace NeuralNetworkSummarization
 
         static IEnumerable<bool> IsInSummary(IEnumerable<string> originalText, IEnumerable<string> summarySentences)
         {
-            var result = new List<bool>( );
+            var result = new List<bool>();
             var summary = summarySentences.Where(s => s != string.Empty);
 
             foreach (var sentence in originalText)
@@ -177,7 +177,7 @@ namespace NeuralNetworkSummarization
 
             return result;
         }
-        static void Main( )
+        static void Main()
         {
             // Load all articles and all summaries
             var allArticles = Directory.GetFiles("Articles");
@@ -188,7 +188,11 @@ namespace NeuralNetworkSummarization
             var stopWords = File.ReadAllText(stopWordsPath);
             var current = 0;
 
-            var allArticleWords = new List<string>( );
+            // Create result folders
+            Directory.CreateDirectory("TrainingData");
+            Directory.CreateDirectory("TestData");
+
+            var allArticleWords = new List<string>();
             Console.WriteLine("Starting...");
             Console.WriteLine($"Processing {allArticles.Length} articles");
 
@@ -196,17 +200,17 @@ namespace NeuralNetworkSummarization
             // Gets all words from all articles
             foreach (var article in allArticles)
             {
-                var text = File.ReadAllText(article).ToLower( );
+                var text = File.ReadAllText(article).ToLower();
                 var trimChars = new [] { ',', '"', '\'', ' ' };
                 var sentences = text.Split(new char[] { '.', '\n' });
                 var notEmpty = sentences.Skip(1).Where(s => s != string.Empty);
                 var sentenceNoWhiteSpace = notEmpty.Select(s => s.Trim(trimChars));
 
                 // Cleaned up sentences, doesn't include stopwords or special characters
-                var cleanedUpSentences = new List<string>( );
+                var cleanedUpSentences = new List<string>();
                 foreach (var sentence in sentenceNoWhiteSpace)
                 {
-                    var tempSentence = new List<string>( );
+                    var tempSentence = new List<string>();
                     var str = sentence.Split(' ').Select(s => s.Trim(trimChars));
                     foreach (var word in str)
                     {
@@ -222,7 +226,7 @@ namespace NeuralNetworkSummarization
                 allArticleWords.AddRange(cleanedWordsInText);
             }
 
-            var importantWords = new Dictionary<string, int>( );
+            var importantWords = new Dictionary<string, int>();
             foreach (var word in allArticleWords)
             {
                 if (!importantWords.ContainsKey(word))
@@ -243,11 +247,11 @@ namespace NeuralNetworkSummarization
             // Generates features for ARFF dataset
             foreach (var article in allArticles)
             {
-                string filePath = article;
-                var text = File.ReadAllText(filePath).ToLower( );
+                var text = File.ReadAllText(article).ToLower();
+                var fileName = Path.GetFileName(article);
 
                 string summaryPath = allSummaries[current];
-                var summary = File.ReadAllText(summaryPath).ToLower( ).Split('.');
+                var summary = File.ReadAllText(summaryPath).ToLower().Split('.');
 
                 // 1. Trim characters from leading or trailing sentences
                 // 2. Split the text as sentences, based on periods or line-jumps
@@ -258,13 +262,13 @@ namespace NeuralNetworkSummarization
                 var sentences = text.Split(new char[] { '.', '\n' });
                 var notEmpty = sentences.Skip(1).Where(s => s != string.Empty);
                 var sentenceNoWhiteSpace = notEmpty.Select(s => s.Trim(trimChars));
-                var title = sentences.First( );
+                var title = sentences.First();
 
                 // Cleaned up sentences, doesn't include stopwords or special characters
-                var cleanedUpSentences = new List<string>( );
+                var cleanedUpSentences = new List<string>();
                 foreach (var sentence in sentenceNoWhiteSpace)
                 {
-                    var tempSentence = new List<string>( );
+                    var tempSentence = new List<string>();
                     var str = sentence.Split(' ').Select(s => s.Trim(trimChars));
                     foreach (var word in str)
                     {
@@ -278,47 +282,50 @@ namespace NeuralNetworkSummarization
                 var cleanedWordsInText = cleanedUpSentences.SelectMany(s => s.Split(' '));
                 var F1 = ExtractFeature1(sentenceNoWhiteSpace, title);
                 var F2 = ExtractFeature2(cleanedUpSentences);
-                var F3 = new List<int>( );
+                var F3 = new List<int>();
                 foreach (var sentence in cleanedUpSentences)
                 {
                     // This has to receive all words in the text
                     F3.Add(ExtractFeature3(cleanedWordsInText, sentence));
                 }
 
-                var F4 = new List<int>( );
+                var F4 = new List<int>();
                 foreach (var sentence in cleanedUpSentences)
                 {
                     F4.Add(ExtractFeature4(sentence));
                 }
 
-                var F5 = new List<bool>( );
+                var F5 = new List<bool>();
                 foreach (var sentence in cleanedUpSentences)
                 {
                     F5.Add(ExtractFeature5(sentence));
                 }
 
-                var F6 = new List<double>( );
+                var F6 = new List<double>();
                 foreach (var sentence in cleanedUpSentences)
                 {
                     F6.Add(ExtractFeature6(sentence, top50Words));
                 }
 
-                var F7 = new List<double>( );
+                var F7 = new List<double>();
                 foreach (var sentence in cleanedUpSentences)
                 {
                     F7.Add(ExtractFeature7(cleanedUpSentences, sentence));
                 }
 
-                var Resultados = IsInSummary(sentenceNoWhiteSpace, summary).ToList( );
+                var Resultados = IsInSummary(sentenceNoWhiteSpace, summary).ToList();
 
-                var lines = new List<string>( );
+                var training = new List<string>();
+                var testing = new List<string>();
 
                 for (int i = 0; i < F1.Count; i++)
                 {
-                    lines.Add($"{F1[i]},{F2[i]},{F3[i]},{F4[i]},{F5[i]},{F6[i]},{F7[i]},{Resultados[i]}");
+                    training.Add($"{F1[i]},{F2[i]},{F3[i]},{F4[i]},{F5[i]},{F6[i]},{F7[i]},{Resultados[i]}");
+                    testing.Add($"{F1[i]},{F2[i]},{F3[i]},{F4[i]},{F5[i]},{F6[i]},{F7[i]},?");
                 }
 
-                File.WriteAllLines($"result{current}.arff", lines);
+                File.WriteAllLines($"training_{current}.arff", training);
+                File.WriteAllLines($"testing_{fileName}.arff", testing);
                 ++current;
             }
 
